4b610c65301ded0ad3d2ed29b3f79fdc9189ea37
Ilona <Ilona_b9ka@mail.ru>
2021-12-23 12:23:29 +0300

    Observable

--- src/creation.js
+++ src/creation.js
@@ -1,4 +1,4 @@
-import {of, from} from 'rxjs';
+import {of, from, Observable} from 'rxjs';
 import {scan} from 'rxjs/operators';
 
 // const stream = of(1, 2 , 3,4);
@@ -7,8 +7,40 @@ import {scan} from 'rxjs/operators';
 //     console.log('value', value)
 // })
 
-const arr = from([1, 2, 3, 4, 5, 6]).pipe(
-    scan((acc, value) => acc.concat(value), [])
-)
+// const arr = from([1, 2, 3, 4, 5, 6]).pipe(
+//     scan((acc, value) => acc.concat(value), [])
+// )
+//
+// arr.subscribe(value => console.log('value', value));
 
-arr.subscribe(value => console.log('value', value));
+const stream = new Observable(observer => {
+
+    observer.next('first value')
+
+    setTimeout(() => observer.next('After 1000ms'), 1000)
+
+    setTimeout(() => observer.complete(), 1500),
+
+    setTimeout(() => observer.error('Something went wrong'), 2000)
+
+    setTimeout(() => observer.next('After 3000ms'), 3000)
+})
+
+//#1
+// stream.subscribe(
+//     (value)=> console.log('value', value),
+//     (error) => console.log(error),
+//     () => console.log('Complete')
+//     )
+
+stream.subscribe({
+    next(value) {
+        console.log(value)
+    },
+    error(error) {
+        console.log(error)
+    },
+    complete() {
+        console.log('complete')
+    }
+})

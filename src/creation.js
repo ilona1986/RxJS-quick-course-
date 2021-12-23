0504a7308381965d030ed08540d251635d9f13fa
Ilona <Ilona_b9ka@mail.ru>
2021-12-23 11:23:10 +0300

    from

--- src/creation.js
+++ src/creation.js
@@ -1,7 +1,14 @@
-import {of} from 'rxjs';
+import {of, from} from 'rxjs';
+import {scan} from 'rxjs/operators';
 
-const stream = of(1, 2 , 3,4);
+// const stream = of(1, 2 , 3,4);
+//
+// stream.subscribe(value => {
+//     console.log('value', value)
+// })
 
-stream.subscribe(value => {
-    console.log('value', value)
-})
+const arr = from([1, 2, 3, 4, 5, 6]).pipe(
+    scan((acc, value) => acc.concat(value), [])
+)
+
+arr.subscribe(value => console.log('value', value));

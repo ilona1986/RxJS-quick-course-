61dff4e51ab72dc09a2bef6c5c804c1b5b29be8a
Ilona <Ilona_b9ka@mail.ru>
2021-12-23 12:45:46 +0300

    fromEvent and canvas

--- src/creation.js
+++ src/creation.js
@@ -1,5 +1,5 @@
-import {of, from, Observable} from 'rxjs';
-import {scan} from 'rxjs/operators';
+import {of, from, Observable, fromEvent} from 'rxjs';
+import {scan, map} from 'rxjs/operators';
 
 // const stream = of(1, 2 , 3,4);
 //
@@ -13,18 +13,18 @@ import {scan} from 'rxjs/operators';
 //
 // arr.subscribe(value => console.log('value', value));
 
-const stream = new Observable(observer => {
-
-    observer.next('first value')
-
-    setTimeout(() => observer.next('After 1000ms'), 1000)
-
-    setTimeout(() => observer.complete(), 1500),
-
-    setTimeout(() => observer.error('Something went wrong'), 2000)
-
-    setTimeout(() => observer.next('After 3000ms'), 3000)
-})
+// const stream = new Observable(observer => {
+//
+//     observer.next('first value')
+//
+//     setTimeout(() => observer.next('After 1000ms'), 1000)
+//
+//     setTimeout(() => observer.complete(), 1500),
+//
+//     setTimeout(() => observer.error('Something went wrong'), 2000)
+//
+//     setTimeout(() => observer.next('After 3000ms'), 3000)
+// })
 
 //#1
 // stream.subscribe(
@@ -33,14 +33,36 @@ const stream = new Observable(observer => {
 //     () => console.log('Complete')
 //     )
 
-stream.subscribe({
-    next(value) {
-        console.log(value)
-    },
-    error(error) {
-        console.log(error)
-    },
-    complete() {
-        console.log('complete')
-    }
+//#2
+// stream.subscribe({
+//     next(value) {
+//         console.log(value)
+//     },
+//     error(error) {
+//         console.log(error)
+//     },
+//     complete() {
+//         console.log('complete')
+//     }
+// })
+
+fromEvent(document.querySelector('canvas'), 'mousemove')
+    .pipe(
+        map(event =>({
+            x: event.offsetX,
+            y: event.offsetY,
+            ctx: event.target.getContext('2d')
+        }))
+    )
+    .subscribe(position => {
+      position.ctx.fillRect(position.x, position.y, 2, 2)
 })
+
+const clear = fromEvent(document.getElementById('clear'),'click')
+
+clear.subscribe(() => {
+    const canvas = document.querySelector('canvas')
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+})
+
+
